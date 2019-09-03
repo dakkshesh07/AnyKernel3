5a91dd0e025ca95f1d905b2f897549eda8a4a99c
Danny Lin <danny@kdrag0n.dev>
2019-09-03 19:23:00 +0530

    flasher: Optimize F2FS file extension list
    Thanks to arter97 for the original script and lists.
    
    Stock list:
        cold file extension:
        jpg
        gif
        png
        avi
        divx
        m4a
        m4v
        m4p
        mp4
        mp3
        3gp
        wmv
        wma
        mpeg
        mkv
        mov
        asx
        asf
        wmx
        svi
        wvx
        wv
        wm
        mpg
        mpe
        rm
        ogg
        opus
        flac
        jpeg
        video
        apk
        so
        exe
        hot file extension:
        db
    md5: 11d3a135e6451d694779a3fbf7a6e9ca
    
    New list:
        cold file extension:
        avi
        divx
        flv
        m2ts
        m4p
        m4v
        mkv
        mov
        ts
        vob
        webm
        3gp
        video
        aac
        ac3
        dts
        flac
        m4a
        mka
        wav
        bmp
        gif
        png
        svg
        webp
        7z
        deb
        gz
        iso
        jar
        lzma
        rar
        tgz
        txz
        udf
        xz
        lz4
        zst
        bz2
        zip
        tar
        pdf
        asx
        asf
        svi
        wvx
        pyc
        ttf
        ttc
        jp
        mp
        og
        wm
        apk
        odex
        vdex
        so
        cnt
        exo
        hot file extension:
        db
    md5: 2766e276237aa435f3d46c2ef1531de0
    
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    (cherry picked from commit 69635f369419422d54e361b1450344017b3f6fb7)
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- anykernel.sh
+++ anykernel.sh
@@ -26,10 +26,67 @@ ramdisk_compression=auto;
 ## AnyKernel install
 dump_boot;
 
-# migrate from /overlay to /overlay.d to enable SAR Magisk
-if [ -d $ramdisk/overlay ]; then
-  rm -rf $ramdisk/overlay;
-fi;
+# begin ramdisk changes
+
+# init.rc
+backup_file init.rc;
+replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+
+# init.tuna.rc
+backup_file init.tuna.rc;
+insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
+append_file init.tuna.rc "bootscript" init.tuna;
+
+# fstab.tuna
+backup_file fstab.tuna;
+patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
+patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
+patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
+append_file fstab.tuna "usbdisk" fstab;
+
+if mountpoint -q /data; then
+  # Optimize F2FS extension list (@arter97)
+  for list_path in $(find /sys/fs/f2fs* -name extension_list); do
+    hash="$(md5sum $list_path | cut -d' ' -f1)"
+
+    # Skip update if our list is already active
+    if [[ $hash == "0e2627998f2f06aa951f90d2af30e859" ]]; then
+      echo "Extension list up-to-date: $list_path"
+      continue
+    fi
+
+    ui_print "  • Optimizing F2FS extension list"
+    echo "Updating extension list: $list_path"
+
+    echo "Clearing extension list"
+
+    hot_count="$(cat $list_path | grep -n 'hot file extension' | cut -d : -f 1)"
+    cold_count="$(($(cat $list_path | wc -l) - $hot_count))"
+
+    cold_list="$(head -n$(($hot_count - 1)) $list_path | grep -v ':')"
+    hot_list="$(tail -n$cold_count $list_path)"
+
+    for ext in $cold_list; do
+      [ ! -z $ext ] && echo "[c]!$ext" > $list_path
+    done
+
+    for ext in $hot_list; do
+      [ ! -z $ext ] && echo "[h]!$ext" > $list_path
+    done
+
+    echo "Writing new extension list"
+
+    for ext in $(cat $home/f2fs-cold.list | grep -v '#'); do
+      [ ! -z $ext ] && echo "[c]$ext" > $list_path
+    done
+
+    for ext in $(cat $home/f2fs-hot.list); do
+      [ ! -z $ext ] && echo "[h]$ext" > $list_path
+    done
+  done
+fi
+
+# end ramdisk changes
 
 write_boot;
 ## end install
